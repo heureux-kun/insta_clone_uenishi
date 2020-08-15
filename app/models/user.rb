84a3aed2e9bf26d668dad855d48e3a8c471976f9
umlautone uenishi <uenishi@umlautone.jp>
2020-08-16 00:17:14 +0900

    ログインはできたがログインした後の画面がまだ表示されていない/よってログアウトもできていない

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@
 #  crypted_password :string(255)
 #  email            :string(255)      not null
 #  salt             :string(255)
+#  username         :string(255)      not null
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #
@@ -13,13 +14,13 @@
 #
 #  index_users_on_email  (email) UNIQUE
 #
+
 class User < ApplicationRecord
-  authenticates_with_sorcery! do |config|
-    config.authentications_class = Authentication
-  end
+  authenticates_with_sorcery!
 
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
