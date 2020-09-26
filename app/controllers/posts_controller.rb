52c5e94420de6a4e76e93179705200f5b69c60f9
umlautone uenishi <uenishi@umlautone.jp>
2020-09-26 16:18:26 +0900

    自分の投稿を編集できるようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: %i[new create edit update destroy]
   def index
-    @posts = Post.all
-  end
-
-  def show
-    @post = Post.find(params[:id])
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
@@ -17,9 +14,28 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to posts_path, success: '投稿しました'
     else
-      flash.now[:danger ] = '投稿に失敗しました'
+      flash.now[:danger] = '投稿に失敗しました'
       render :new
     end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    # ここの書き方が全然思いつかなかった
+    if @post.update(post_params)
+      redirect_to posts_path, success: '投稿を更新しました'
+    else
+      flash.now[:danger] = '投稿の更新に失敗しました'
+      render :edit
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   # ここの書き方全然思いつかなかった
@@ -28,6 +44,8 @@ class PostsController < ApplicationController
     @post.destroy!
     redirect_to posts_path, success: '投稿を削除しました'
   end
+
+  private
 
   def post_params
     params.require(:post).permit(:body, images: [])
